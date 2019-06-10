faf9c99032dfbaeb7c553e516f8005193151a680
Sirko2097 <2097_Cthusq>
2019-06-10 18:26:54 +0300

    Test connection to DB.

--- src/main/java/com/bankproject/Main.java
+++ src/main/java/com/bankproject/Main.java
@@ -7,9 +7,9 @@ import java.sql.SQLException;
 public class Main {
 
     public static void main(String[] args) {
-        String url = "jdbc:postgresql://localhost:5432/postgres";
-        String user = "sirko";
-        String password = "2097_Cthusq";
+        String url = "jdbc:oracle:thin:@localhost:1521:XE";
+        String user = "SIRKO";
+        String password = "S5116951169";
 
         try (Connection connection = DriverManager.getConnection(url, user, password)) {
             System.out.println("Connection successful!");
